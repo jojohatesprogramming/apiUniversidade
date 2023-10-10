8243cc57e1ae488f0d8377d91ea140889fd41e4b
jojohatesprogramming <joatan.p@escolar.ifrn.edu.br>
2023-10-10 11:47:57 -0300

    Segundo Commit

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiUniversidade.Context;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +13,16 @@ namespace apiUniversidade.Controllers
     public class CursoController : Controller
     {
         private readonly ILogger<CursoController> _logger;
+        private readonly apiUniversidadeContext _context;
 
-        public CursoController(ILogger<CursoController> logger)
+        public CursoController(ILogger<CursoController> logger, apiUniversidadeContext context)
         {
             _logger = logger;
+            _context = context;
         }
         [HttpGet]
         public ActionResult<IEnumerable<Curso>> Get(){
-            var cursos = context.Cursos.ToList();
+            var cursos = _context.Cursos.ToList();
             if (cursos is null)
                 return NotFound();
 
